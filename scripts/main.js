06a7423c2941afb5e5b6097cff367f38bf23255b
Wes Reid <bwreid@gmail.com>
2017-11-02 17:11:01 -0700

    Update the baseURL to correctly pick Heroku version

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const localhostURL = 'http://localhost:3000/posts'
 const herokuURL = 'https://ajax-blog-api.herokuapp.com/posts'
-const baseURL = window.location.href.includes('localhost') ? localhostURL : herokuURL
+const baseURL = window.location.href.includes('herokuapp') ? herokuURL : localhostURL
 
 homeView.init()
 
